a4b728cf6d65c1e66528399bc36731924dce10ad
mattyost00 <72537294+mattyost00@users.noreply.github.com>
2022-02-15 17:48:07 -0700

    Nearly  finished sign up page

---
+++ lib/Screens/SignUp/components/body.dart
@@ -0,0 +1,64 @@
+import 'package:flutter/material.dart';
+import 'package:sign_ups/Components/rounded_input_field.dart';
+import 'package:sign_ups/Components/rounded_password_field.dart';
+import 'package:sign_ups/Screens/SignUp/components/signUpBackground.dart';
+
+class Body extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    Size size = MediaQuery.of(context).size;
+    return Background(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: <Widget>[
+          Text(
+            "Let the Games Begin",
+            style: TextStyle(
+              fontWeight: FontWeight.normal,
+              fontSize: 30,
+            ),
+          ),
+          SizedBox(height: 20),
+          Row(mainAxisAlignment: MainAxisAlignment.start, children: <Widget>[
+            Spacer(flex: 1),
+            Text(
+              "Username",
+              style: TextStyle(
+                  fontWeight: FontWeight.bold), //TODO: change this font
+            ),
+            Spacer(flex: 5),
+          ]),
+          RoundedInputField(
+            hintText: "Your Username",
+            onChanged: (value) {},
+          ),
+          Row(mainAxisAlignment: MainAxisAlignment.start, children: <Widget>[
+            //TODO:Figure out how to shift this to the right to match figma
+            Spacer(flex: 1),
+            Text(
+              "Password",
+              style: TextStyle(fontWeight: FontWeight.bold),
+            ),
+            Spacer(flex: 5)
+          ]),
+          RoundedPasswordField(
+            onChanged: (value) {},
+          ),
+          Row(mainAxisAlignment: MainAxisAlignment.start, children: <Widget>[
+            //TODO:Figure out how to shift this to the right to match figma
+            Spacer(flex: 1),
+            Text(
+              "Password",
+              style: TextStyle(fontWeight: FontWeight.bold),
+            ),
+            Spacer(flex: 5)
+          ]),
+          RoundedPasswordField(
+            onChanged: (value) {},
+          ),
+          SizedBox(height: size.height * .2),
+        ],
+      ),
+    );
+  }
+}
