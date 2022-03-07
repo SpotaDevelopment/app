8f4b466edbc05606bf642cd39ef59979c81db25b
tbcurtis <tbcurtis20@gmail.com>
2022-03-06 22:01:20 -0700

    added active icons for the bottom nav bar

--- lib/Components/bottom_navigation_bar.dart
+++ lib/Components/bottom_navigation_bar.dart
@@ -1,13 +1,13 @@
 import 'package:flutter/material.dart';
 
-class BottomNavBar extends StatefulWidget{
+class BottomNavBar extends StatefulWidget {
   const BottomNavBar({Key? key}) : super(key: key);
 
   @override
   State<BottomNavBar> createState() => _BottomNavBarState();
 }
 
-class _BottomNavBarState extends State<BottomNavBar>{
+class _BottomNavBarState extends State<BottomNavBar> {
   int _selectedIndex = 2;
   void _onItemTapped(int index) {
     setState(() {
@@ -17,48 +17,43 @@ class _BottomNavBarState extends State<BottomNavBar>{
 
   @override
   Widget build(BuildContext context) {
-    type: BottomNavigationBarType.fixed;
     return BottomNavigationBar(
-        items: const <BottomNavigationBarItem>[
-          BottomNavigationBarItem(
-            icon: Icon(Icons.newspaper,
-                color: Colors.black,
-                size: 32),
-            backgroundColor: Colors.white,
-            label: '',
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.scoreboard,
-                color: Colors.black,
-                size: 32),
-            backgroundColor: Colors.white,
-            label: '',
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.home,
-                color: Colors.black,
-                size: 32),
-            backgroundColor: Colors.white,
-            label: '',
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.chat,
-                color: Colors.black,
-                size: 32),
-            backgroundColor: Colors.white,
-            label: '',
-          ),
-          BottomNavigationBarItem(
-              icon: Icon(Icons.person,
-                  color: Colors.black,
-                  size: 32),
-              backgroundColor: Colors.white,
-              label: ''
-          ),
-        ],
-        currentIndex: _selectedIndex,
-        selectedItemColor: Colors.amber[800],
-        onTap: _onItemTapped,
+      type: BottomNavigationBarType.fixed,
+      items: const <BottomNavigationBarItem>[
+        BottomNavigationBarItem(
+          icon: Icon(Icons.newspaper, color: Colors.black, size: 32),
+          backgroundColor: Colors.white,
+          label: 'News',
+          activeIcon: Icon(Icons.newspaper, color: Colors.amber, size: 32),
+        ),
+        BottomNavigationBarItem(
+          icon: Icon(Icons.scoreboard, color: Colors.black, size: 32),
+          backgroundColor: Colors.white,
+          label: 'Scores',
+          activeIcon: Icon(Icons.scoreboard, color: Colors.amber, size: 32),
+        ),
+        BottomNavigationBarItem(
+          icon: Icon(Icons.home, color: Colors.black, size: 32),
+          backgroundColor: Colors.white,
+          label: 'Home',
+          activeIcon: Icon(Icons.home, color: Colors.amber, size: 32),
+        ),
+        BottomNavigationBarItem(
+          icon: Icon(Icons.chat, color: Colors.black, size: 32),
+          backgroundColor: Colors.white,
+          label: 'Chat',
+          activeIcon: Icon(Icons.chat, color: Colors.amber, size: 32),
+        ),
+        BottomNavigationBarItem(
+          icon: Icon(Icons.person, color: Colors.black, size: 32),
+          backgroundColor: Colors.white,
+          label: 'Profile',
+          activeIcon: Icon(Icons.person, color: Colors.amber, size: 32),
+        ),
+      ],
+      currentIndex: _selectedIndex,
+      selectedItemColor: Colors.amber[800],
+      onTap: _onItemTapped,
     );
   }
 }
