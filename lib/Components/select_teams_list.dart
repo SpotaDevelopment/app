d6072e93c2f852421b7f2d71a54efc941f2755ab
mattyost00 <72537294+mattyost00@users.noreply.github.com>
2022-03-10 23:43:03 -0700

    Fixed erxpansion tile color

--- lib/Components/select_teams_list.dart
+++ lib/Components/select_teams_list.dart
@@ -38,46 +38,54 @@ class _SelectTeamsListState extends State<SelectTeamsList> {
 
   Widget buildTeamList(var leagueAndTeam) {
     Size size = MediaQuery.of(context).size;
-    return ExpansionTile(
-      title: Text(
-        leagueAndTeam['league'],
-        style: TextStyle(
-          fontFamily: "Oxanium",
-          fontWeight: FontWeight.bold,
-          fontSize: 17,
-        ),
-      ),
-      children: [
-        Container(
-          color: lightGrey,
-          child: ListView.separated(
-            scrollDirection: Axis.vertical,
-            shrinkWrap: true,
-            primary: false,
-            itemCount: leagueAndTeam['IconsAndTeams'].length + 1,
-            itemBuilder: (context, index) {
-              if (index == 0) return const SizedBox.shrink();
-              return SizedBox(
-                height: size.height * 0.065,
-                child: ListTile(
-                  leading: leagueAndTeam['IconsAndTeams'][index - 1]['icon'],
-                  title:
-                      Text(leagueAndTeam['IconsAndTeams'][index - 1]['team']),
-                  tileColor: lightGrey,
-                ),
-              );
-            },
-            separatorBuilder: (context, index) {
-              print(leagueAndTeam['IconsAndTeams'].length);
-              return Divider(
-                color: Colors.black,
-                thickness: 1,
-                height: 1,
-              );
-            },
+    return Container(
+      color: Colors.grey,
+      child: ExpansionTile(
+        title: Text(
+          leagueAndTeam['league'],
+          style: TextStyle(
+            fontFamily: "Oxanium",
+            fontWeight: FontWeight.bold,
+            fontSize: 17,
           ),
-        )
-      ],
+        ),
+        children: [
+          Container(
+            color: lightGrey,
+            child: ListView.separated(
+              scrollDirection: Axis.vertical,
+              shrinkWrap: true,
+              primary: false,
+              itemCount: leagueAndTeam['IconsAndTeams'].length + 1,
+              itemBuilder: (context, index) {
+                if (index == 0) return const SizedBox.shrink();
+                return SizedBox(
+                  height: size.height * 0.065,
+                  child: ListTile(
+                    leading: leagueAndTeam['IconsAndTeams'][index - 1]['icon'],
+                    title:
+                        Text(leagueAndTeam['IconsAndTeams'][index - 1]['team']),
+                    tileColor: lightGrey,
+                  ),
+                );
+              },
+              separatorBuilder: (context, index) {
+                return Divider(
+                  color: Colors.black,
+                  thickness: 1,
+                  height: 1,
+                );
+              },
+            ),
+          )
+        ],
+        onExpansionChanged: (changed) {
+          iconColor:
+          Colors.black;
+          textColor:
+          Colors.black;
+        },
+      ),
     );
   }
 }
