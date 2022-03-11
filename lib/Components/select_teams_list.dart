b5528b5c03e85363367cd6c3644146b3fe067e04
mattyost00 <72537294+mattyost00@users.noreply.github.com>
2022-03-10 23:52:06 -0700

    Fixed issue with ExpansionTile text color and trailing icon color

--- lib/Components/select_teams_list.dart
+++ lib/Components/select_teams_list.dart
@@ -40,51 +40,64 @@ class _SelectTeamsListState extends State<SelectTeamsList> {
     Size size = MediaQuery.of(context).size;
     return Container(
       color: Colors.grey,
-      child: ExpansionTile(
-        title: Text(
-          leagueAndTeam['league'],
-          style: TextStyle(
-            fontFamily: "Oxanium",
-            fontWeight: FontWeight.bold,
-            fontSize: 17,
-          ),
+      child: Theme(
+        data: Theme.of(context).copyWith(
+          unselectedWidgetColor: Colors.black, // here for close state
+          colorScheme: ColorScheme.light(
+            primary: Colors.black,
+          ), // here for open state in replacement of deprecated accentColor
+          dividerColor: Colors.transparent, // if you want to remove the border
         ),
-        children: [
-          Container(
-            color: lightGrey,
-            child: ListView.separated(
-              scrollDirection: Axis.vertical,
-              shrinkWrap: true,
-              primary: false,
-              itemCount: leagueAndTeam['IconsAndTeams'].length + 1,
-              itemBuilder: (context, index) {
-                if (index == 0) return const SizedBox.shrink();
-                return SizedBox(
-                  height: size.height * 0.065,
-                  child: ListTile(
-                    leading: leagueAndTeam['IconsAndTeams'][index - 1]['icon'],
-                    title:
-                        Text(leagueAndTeam['IconsAndTeams'][index - 1]['team']),
-                    tileColor: lightGrey,
-                  ),
-                );
-              },
-              separatorBuilder: (context, index) {
-                return Divider(
-                  color: Colors.black,
-                  thickness: 1,
-                  height: 1,
-                );
-              },
+        child: ExpansionTile(
+          title: Container(
+            child: Text(
+              leagueAndTeam['league'],
+              style: TextStyle(
+                fontFamily: "Oxanium",
+                fontWeight: FontWeight.bold,
+                fontSize: 17,
+                color: Colors.black,
+              ),
             ),
-          )
-        ],
-        onExpansionChanged: (changed) {
-          iconColor:
-          Colors.black;
-          textColor:
-          Colors.black;
-        },
+          ),
+          children: [
+            Container(
+              color: lightGrey,
+              child: ListView.separated(
+                scrollDirection: Axis.vertical,
+                shrinkWrap: true,
+                primary: false,
+                itemCount: leagueAndTeam['IconsAndTeams'].length + 1,
+                itemBuilder: (context, index) {
+                  if (index == 0) return const SizedBox.shrink();
+                  return SizedBox(
+                    height: size.height * 0.065,
+                    child: ListTile(
+                      leading: leagueAndTeam['IconsAndTeams'][index - 1]
+                          ['icon'],
+                      title: Text(
+                          leagueAndTeam['IconsAndTeams'][index - 1]['team']),
+                      tileColor: lightGrey,
+                    ),
+                  );
+                },
+                separatorBuilder: (context, index) {
+                  return Divider(
+                    color: Colors.black,
+                    thickness: 1,
+                    height: 1,
+                  );
+                },
+              ),
+            )
+          ],
+          onExpansionChanged: (changed) {
+            iconColor:
+            Colors.black;
+            textColor:
+            Colors.black;
+          },
+        ),
       ),
     );
   }
