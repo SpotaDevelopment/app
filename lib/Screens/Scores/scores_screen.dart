a03bf9a3ad2acc0e9cbef1fb8d11a054af1d287a
tbcurtis <tbcurtis20@gmail.com>
2022-03-11 00:41:12 -0700

    added fetch games call

--- lib/Screens/Scores/scores_screen.dart
+++ lib/Screens/Scores/scores_screen.dart
@@ -8,10 +8,24 @@ import '../../Components/ScoreCards/Basketball/future_basketball_score_card.dart
 import '../../Components/ScoreCards/Basketball/live_basketball_score_card.dart';
 import '../../Components/bottom_navigation_bar.dart';
 import '../../Components/news_article_without_image.dart';
+import '../../model/Game.dart';
 
-class SportsScoresPage extends StatelessWidget{
+class SportsScoresPage extends StatefulWidget {
   static const String path = 'lib/Screens/Scores/scores_screen.dart';
   SportsScoresPage({Key? key}) : super(key: key);
+
+  @override
+  _SportsScoresPageState createState() => _SportsScoresPageState();
+}
+
+class _SportsScoresPageState extends State<SportsScoresPage> {
+  late Future<List<Game>> futureGames;
+
+  @override
+  void initState() {
+    super.initState();
+    //futureGames = fetchGames();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -23,36 +37,66 @@ class SportsScoresPage extends StatelessWidget{
             padding: const EdgeInsets.only(top: 8.0),
             child: LeaguesToggleButtons(),
           ),
-          const Divider(
-              color: Colors.black),
+          const Divider(color: Colors.black),
           Expanded(
             child: ListView(
               padding: const EdgeInsets.all(8),
               children: const <Widget>[
-                FinalBasketballScoreCard(homeTeam: "PHX",
-                  homeRecord: "51-13", homeScore: "111", awayTeam: "BOS",
-                awayRecord: "39-27", awayScore: "90", date: "12/10/21"),
-                FinalBasketballScoreCard(homeTeam: "DEN",
-                    homeRecord: "39-26", homeScore: "131", awayTeam: "GSW",
-                    awayRecord: "43-22", awayScore: "124", date: "Yesterday"),
-                LiveBasketballScoreCard(homeTeam: "OKC",
-                    homeRecord: "20-44", homeScore: "61", awayTeam: "MIL",
-                    awayRecord: "40-25", awayScore: "68", period: "Q2", time: "2:21"),
-                LiveBasketballScoreCard(homeTeam: "MEM",
-                    homeRecord: "44-22", homeScore: "77", awayTeam: "NOP",
-                    awayRecord: "27-37", awayScore: "58", period: "Halftime"),
-                FutureBasketballScoreCard(homeTeam: "MIA",
-                    homeRecord: "52-13", awayTeam: "PHX",
-                    awayRecord: "44-22", date: "Tomorrow", time: "5:30 PM",),
-                FutureBasketballScoreCard(homeTeam: "BOS",
-                  homeRecord: "39-27", awayTeam: "DET",
-                  awayRecord: "18-47", date: "3/11/22", time: "5:30 PM",),
+                FinalBasketballScoreCard(
+                    homeTeam: "PHX",
+                    homeRecord: "51-13",
+                    homeScore: "111",
+                    awayTeam: "BOS",
+                    awayRecord: "39-27",
+                    awayScore: "90",
+                    date: "12/10/21"),
+                FinalBasketballScoreCard(
+                    homeTeam: "DEN",
+                    homeRecord: "39-26",
+                    homeScore: "131",
+                    awayTeam: "GSW",
+                    awayRecord: "43-22",
+                    awayScore: "124",
+                    date: "Yesterday"),
+                LiveBasketballScoreCard(
+                    homeTeam: "OKC",
+                    homeRecord: "20-44",
+                    homeScore: "61",
+                    awayTeam: "MIL",
+                    awayRecord: "40-25",
+                    awayScore: "68",
+                    period: "Q2",
+                    time: "2:21"),
+                LiveBasketballScoreCard(
+                    homeTeam: "MEM",
+                    homeRecord: "44-22",
+                    homeScore: "77",
+                    awayTeam: "NOP",
+                    awayRecord: "27-37",
+                    awayScore: "58",
+                    period: "Halftime"),
+                FutureBasketballScoreCard(
+                  homeTeam: "MIA",
+                  homeRecord: "52-13",
+                  awayTeam: "PHX",
+                  awayRecord: "44-22",
+                  date: "Tomorrow",
+                  time: "5:30 PM",
+                ),
+                FutureBasketballScoreCard(
+                  homeTeam: "BOS",
+                  homeRecord: "39-27",
+                  awayTeam: "DET",
+                  awayRecord: "18-47",
+                  date: "3/11/22",
+                  time: "5:30 PM",
+                ),
               ],
             ),
           ),
         ],
       ),
-      bottomNavigationBar: BottomNavBar(),
+      bottomNavigationBar: const BottomNavBar(),
     );
   }
 }
--- lib/model/Game.dart
+++ lib/model/Game.dart
@@ -1,40 +1,151 @@
+import 'dart:convert';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:http/http.dart' as http;
+
+import '../constants.dart';
+
 class Game {
-  String homeTeamName;
-  String awayTeamName;
-  String homeTeamAbrv;
-  String awayTeamAbrv;
-  String date;
-  int homeTeamScore;
-  int awayTeamScore;
-  int homeTeamWins;
-  int homeTeamLosses;
-  int awayTeamWins;
-  int awayTeamLosses;
+  final String homeTeamName;
+  final String homeTeamAbrv;
+  final String awayTeamName;
+  final String awayTeamAbrv;
+  final String date;
+  final int homeTeamScore;
+  final int awayTeamScore;
+  final int homeTeamWins;
+  final int homeTeamLosses;
+  final int awayTeamWins;
+  final int awayTeamLosses;
+  const Game({
+    required this.homeTeamName,
+    required this.homeTeamAbrv,
+    required this.awayTeamName,
+    required this.awayTeamAbrv,
+    required this.date,
+    required this.homeTeamScore,
+    required this.awayTeamScore,
+    required this.homeTeamWins,
+    required this.homeTeamLosses,
+    required this.awayTeamWins,
+    required this.awayTeamLosses,
+  });
 
-  Game(
-      {required this.homeTeamName,
-      required this.awayTeamName,
-      required this.homeTeamAbrv,
-      required this.awayTeamAbrv,
-      required this.date,
-      required this.homeTeamScore,
-      required this.awayTeamScore,
-      required this.homeTeamWins,
-      required this.homeTeamLosses,
-      required this.awayTeamWins,
-      required this.awayTeamLosses});
+  Game copyWith({
+    String? homeTeamName,
+    String? homeTeamAbrv,
+    String? awayTeamName,
+    String? awayTeamAbrv,
+    String? date,
+    int? homeTeamScore,
+    int? awayTeamScore,
+    int? homeTeamWins,
+    int? homeTeamLosses,
+    int? awayTeamWins,
+    int? awayTeamLosses,
+  }) {
+    return Game(
+      homeTeamName: homeTeamName ?? this.homeTeamName,
+      homeTeamAbrv: homeTeamAbrv ?? this.homeTeamAbrv,
+      awayTeamName: awayTeamName ?? this.awayTeamName,
+      awayTeamAbrv: awayTeamAbrv ?? this.awayTeamAbrv,
+      date: date ?? this.date,
+      homeTeamScore: homeTeamScore ?? this.homeTeamScore,
+      awayTeamScore: awayTeamScore ?? this.awayTeamScore,
+      homeTeamWins: homeTeamWins ?? this.homeTeamWins,
+      homeTeamLosses: homeTeamLosses ?? this.homeTeamLosses,
+      awayTeamWins: awayTeamWins ?? this.awayTeamWins,
+      awayTeamLosses: awayTeamLosses ?? this.awayTeamLosses,
+    );
+  }
 
-  Map toJson() => {
-        'homeTeamName': homeTeamName,
-        'awayTeamName': awayTeamName,
-        'homeTeamAbrv': homeTeamAbrv,
-        'awayTeamAbrv': awayTeamAbrv,
-        'date': date,
-        'homeTeamScore': homeTeamScore,
-        'awayTeamScore': awayTeamScore,
-        'homeTeamWins': homeTeamWins,
-        'homeTeamLosses': homeTeamLosses,
-        'awayTeamWins': awayTeamWins,
-        'awayTeamLosses': awayTeamLosses
-      };
+  Map<String, dynamic> toMap() {
+    return {
+      'homeTeamName': homeTeamName,
+      'homeTeamAbrv': homeTeamAbrv,
+      'awayTeamName': awayTeamName,
+      'awayTeamAbrv': awayTeamAbrv,
+      'date': date,
+      'homeTeamScore': homeTeamScore,
+      'awayTeamScore': awayTeamScore,
+      'homeTeamWins': homeTeamWins,
+      'homeTeamLosses': homeTeamLosses,
+      'awayTeamWins': awayTeamWins,
+      'awayTeamLosses': awayTeamLosses,
+    };
+  }
+
+  factory Game.fromMap(Map<String, dynamic> map) {
+    return Game(
+      homeTeamName: map['homeTeamName'] ?? '',
+      homeTeamAbrv: map['homeTeamAbrv'] ?? '',
+      awayTeamName: map['awayTeamName'] ?? '',
+      awayTeamAbrv: map['awayTeamAbrv'] ?? '',
+      date: map['date'] ?? '',
+      homeTeamScore: map['homeTeamScore']?.toInt() ?? 0,
+      awayTeamScore: map['awayTeamScore']?.toInt() ?? 0,
+      homeTeamWins: map['homeTeamWins']?.toInt() ?? 0,
+      homeTeamLosses: map['homeTeamLosses']?.toInt() ?? 0,
+      awayTeamWins: map['awayTeamWins']?.toInt() ?? 0,
+      awayTeamLosses: map['awayTeamLosses']?.toInt() ?? 0,
+    );
+  }
+
+  String toJson() => json.encode(toMap());
+
+  factory Game.fromJson(String source) => Game.fromMap(json.decode(source));
+
+  @override
+  String toString() {
+    return 'Game(homeTeamName: $homeTeamName, homeTeamAbrv: $homeTeamAbrv, awayTeamName: $awayTeamName, awayTeamAbrv: $awayTeamAbrv, date: $date, homeTeamScore: $homeTeamScore, awayTeamScore: $awayTeamScore, homeTeamWins: $homeTeamWins, homeTeamLosses: $homeTeamLosses, awayTeamWins: $awayTeamWins, awayTeamLosses: $awayTeamLosses)';
+  }
+
+  @override
+  bool operator ==(Object other) {
+    if (identical(this, other)) return true;
+
+    return other is Game &&
+        other.homeTeamName == homeTeamName &&
+        other.homeTeamAbrv == homeTeamAbrv &&
+        other.awayTeamName == awayTeamName &&
+        other.awayTeamAbrv == awayTeamAbrv &&
+        other.date == date &&
+        other.homeTeamScore == homeTeamScore &&
+        other.awayTeamScore == awayTeamScore &&
+        other.homeTeamWins == homeTeamWins &&
+        other.homeTeamLosses == homeTeamLosses &&
+        other.awayTeamWins == awayTeamWins &&
+        other.awayTeamLosses == awayTeamLosses;
+  }
+
+  @override
+  int get hashCode {
+    return homeTeamName.hashCode ^
+        homeTeamAbrv.hashCode ^
+        awayTeamName.hashCode ^
+        awayTeamAbrv.hashCode ^
+        date.hashCode ^
+        homeTeamScore.hashCode ^
+        awayTeamScore.hashCode ^
+        homeTeamWins.hashCode ^
+        homeTeamLosses.hashCode ^
+        awayTeamWins.hashCode ^
+        awayTeamLosses.hashCode;
+  }
+}
+
+Future<List<Game>> fetchGames() async {
+  var user_email = "";
+  if (FirebaseAuth.instance.currentUser != null) {
+    user_email = FirebaseAuth.instance.currentUser!.email.toString();
+  } else {
+    //TODO: no user logged in
+  }
+  final response = await http.get(
+      Uri.parse("http://" + serverDomain + "users/getScores/" + user_email));
+  if (response.statusCode == 200) {
+    Iterable l = json.decode(response.body);
+    return List<Game>.from(l.map((model) => Game.fromJson(model)));
+  } else {
+    throw Exception('Failed to load games');
+  }
 }
