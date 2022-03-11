aae75f953f92198099498fa3d070f0e82b65818e
Griffin Bourdon <gcbourdon@gmail.com>
2022-03-10 19:28:15 -0700

    added game object

---
+++ lib/model/Game.dart
@@ -0,0 +1,40 @@
+class Game {
+  String homeTeamName;
+  String awayTeamName;
+  String homeTeamAbrv;
+  String awayTeamAbrv;
+  String date;
+  int homeTeamScore;
+  int awayTeamScore;
+  int homeTeamWins;
+  int homeTeamLosses;
+  int awayTeamWins;
+  int awayTeamLosses;
+
+  Game(
+      {required this.homeTeamName,
+      required this.awayTeamName,
+      required this.homeTeamAbrv,
+      required this.awayTeamAbrv,
+      required this.date,
+      required this.homeTeamScore,
+      required this.awayTeamScore,
+      required this.homeTeamWins,
+      required this.homeTeamLosses,
+      required this.awayTeamWins,
+      required this.awayTeamLosses});
+
+  Map toJson() => {
+        'homeTeamName': homeTeamName,
+        'awayTeamName': awayTeamName,
+        'homeTeamAbrv': homeTeamAbrv,
+        'awayTeamAbrv': awayTeamAbrv,
+        'date': date,
+        'homeTeamScore': homeTeamScore,
+        'awayTeamScore': awayTeamScore,
+        'homeTeamWins': homeTeamWins,
+        'homeTeamLosses': homeTeamLosses,
+        'awayTeamWins': awayTeamWins,
+        'awayTeamLosses': awayTeamLosses
+      };
+}
