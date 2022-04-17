0bf703a752dd49216bcbe6cfcfe0df03c9ab37b6
mattyost00 <72537294+mattyost00@users.noreply.github.com>
2022-04-17 02:35:44 -0700

    Added user services folder

---
+++ lib/UserServices/userServices.dart
@@ -0,0 +1,41 @@
+import 'dart:convert';
+import 'dart:io';
+
+import 'package:sign_ups/model/UserAccount.dart';
+
+import '../constants/backend_constants.dart';
+import 'package:http/http.dart' as http;
+
+import '../model/Game.dart';
+
+Future<List<UserAccount>> getUsersByPrefixService(
+    String prefix, bool isEmail) async {
+  String field = (isEmail) ? "email" : "username";
+  var url = Uri.parse(
+      serverDomain + "users/getUsersByPrefix/" + prefix + "/" + field);
+  final response = await http.get(url);
+  if (response.statusCode != 200) {
+    print(response.statusCode);
+    print(response.body);
+    throw Exception(response.statusCode);
+  }
+  print(response.body);
+  List<UserAccount> userAccounts = (json.decode(response.body) as List)
+      .map((i) => UserAccount.fromJson(i))
+      .toList();
+  return userAccounts;
+}
+
+Future<UserAccount?> getUserAccountByEmail(String email) async {
+  var url = Uri.parse(serverDomain + "users/getUserByEmail/" + email);
+  final response = await http.get(url);
+  if (response.statusCode != 200) {
+    print(response.statusCode);
+    print(response.body);
+    throw Exception(response.statusCode);
+  }
+  print(response.body);
+  UserAccount userAccount =
+      (json.decode(response.body)).map((i) => UserAccount.fromJson(i));
+  return userAccount;
+}
