8467be980c4108f4f5ee37482f5cf161c78865fc
tbcurtis <tbcurtis20@gmail.com>
2022-04-19 21:36:03 -0700

    removed headerbar from main

--- lib/main.dart
+++ lib/main.dart
@@ -59,22 +59,4 @@ class _MyAppState extends State<MyApp> {
       home: WelcomeScreen(),
     );
   }
-
-  AppBar headerBar() {
-    return AppBar(
-      title: Text('Spota',
-          style: TextStyle(
-            fontFamily: "Oxanium",
-            fontSize: 30,
-          )),
-      backgroundColor: Color(0xffff7a22),
-      actions: const <Widget>[
-        Icon(
-          Icons.menu,
-          color: Colors.white,
-          size: 24.0,
-        )
-      ],
-    );
-  }
 }
