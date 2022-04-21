e8e293776ad01626c0c47d9b8d38327b1c9c4cfe
mattyost00 <72537294+mattyost00@users.noreply.github.com>
2022-04-20 20:21:47 -0700

    Fixed overflow on login page

--- lib/Screens/Login/components/body.dart
+++ lib/Screens/Login/components/body.dart
@@ -31,117 +31,131 @@ class Body extends StatelessWidget {
   Widget build(BuildContext context) {
     Size size = MediaQuery.of(context).size;
     return Background(
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: <Widget>[
-          Padding(
-            padding: EdgeInsets.only(top: (size.height * .3), bottom: 15),
-            child: Text(
-              "Welcome Back",
-              style: TextStyle(
-                  fontFamily: "Oxanium",
-                  fontWeight: FontWeight.normal,
-                  fontSize: 30),
-            ),
-          ),
-          Row(mainAxisAlignment: MainAxisAlignment.start, children: <Widget>[
-            Padding(
-              padding: EdgeInsets.only(left: (size.width * .1)),
-              child: Text(
-                "Email",
-                style: TextStyle(
-                    fontFamily: "Oxanium", fontWeight: FontWeight.bold),
-              ),
-            ),
-          ]),
-          RoundedInputField(
-            controller: emailController,
-            hintText: "Your Email",
-            onChanged: (value) {},
-            icon: Icons.person,
-          ),
-          Row(mainAxisAlignment: MainAxisAlignment.start, children: <Widget>[
-            Padding(
-              padding: EdgeInsets.only(left: (size.width * .1)),
-              child: Text(
-                "Password",
-                style: TextStyle(
-                    fontFamily: "Oxanium", fontWeight: FontWeight.bold),
-              ),
-            ),
-          ]),
-          RoundedPasswordField(
-            controller: passwordController,
-            // onChanged: (value) {},
-          ),
-          GestureDetector(
-            onTap: () {
-              Navigator.push(
-                context,
-                MaterialPageRoute(
-                  builder: (context) {
-                    return ForgotInfoScreen(); //TODO: Change this to redirect to the choose teams page once its setup
-                  },
+      child: SingleChildScrollView(
+        child: SizedBox(
+          height: size.height,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              Padding(
+                padding: EdgeInsets.only(top: (size.height * .3), bottom: 15),
+                child: Text(
+                  "Welcome Back",
+                  style: TextStyle(
+                      fontFamily: "Oxanium",
+                      fontWeight: FontWeight.normal,
+                      fontSize: 30),
                 ),
-              );
-            }, //TODO: Add in the transition to forgot username/password page
-            child: Text(
-              "Forgot Username or Password?",
-              style: TextStyle(
-                decoration: TextDecoration.underline,
               ),
-            ),
-          ),
-          Expanded(
-            child: Align(
-              alignment: Alignment.bottomCenter,
-              child: RoundedButton(
-                text: "Login",
-                pressed: () {
-                  AuthenticationService()
-                      .signIn(
-                          email: emailController.text.trim(),
-                          password: passwordController.text.trim())
-                      .then((result) async {
-                    if (result == null) {
-                      globalUserAccount = await getUserAccountByEmail(
-                          emailController.text.trim());
-                      Navigator.pushReplacement(context,
-                          MaterialPageRoute(builder: (context) => HomePage()));
-                    } else {
-                      // Handling error display
-                      ScaffoldMessenger.of(context).showSnackBar(
-                        SnackBar(
-                          content: Text(
-                            result,
-                            style: TextStyle(fontSize: 16),
-                          ),
-                        ),
-                      );
-                    }
-                  });
-                },
-                color: Colors.black,
-                textColor: Colors.white,
+              Row(
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  children: <Widget>[
+                    Padding(
+                      padding: EdgeInsets.only(left: (size.width * .1)),
+                      child: Text(
+                        "Email",
+                        style: TextStyle(
+                            fontFamily: "Oxanium", fontWeight: FontWeight.bold),
+                      ),
+                    ),
+                  ]),
+              RoundedInputField(
+                controller: emailController,
+                hintText: "Your Email",
+                onChanged: (value) {},
+                icon: Icons.person,
               ),
-            ),
-          ),
-          Padding(
-            padding: EdgeInsets.only(bottom: (size.height * .05)),
-            child: AlreadyHaveAnAccountCheck(
-                login: true,
-                press: () {
+              Row(
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  children: <Widget>[
+                    Padding(
+                      padding: EdgeInsets.only(left: (size.width * .1)),
+                      child: Text(
+                        "Password",
+                        style: TextStyle(
+                            fontFamily: "Oxanium", fontWeight: FontWeight.bold),
+                      ),
+                    ),
+                  ]),
+              RoundedPasswordField(
+                controller: passwordController,
+                // onChanged: (value) {},
+              ),
+              GestureDetector(
+                onTap: () {
                   Navigator.push(
                     context,
                     MaterialPageRoute(
                       builder: (context) {
-                        return SignUpScreen(); //TODO: Change this to redirect to the choose teams page once its setup
+                        return ForgotInfoScreen(); //TODO: Change this to redirect to the choose teams page once its setup
                       },
                     ),
                   );
-                }),
+                }, //TODO: Add in the transition to forgot username/password page
+                child: Text(
+                  "Forgot Username or Password?",
+                  style: TextStyle(
+                    decoration: TextDecoration.underline,
+                  ),
+                ),
+              ),
+              Expanded(
+                child: Align(
+                  alignment: Alignment.bottomCenter,
+                  child: RoundedButton(
+                    text: "Login",
+                    pressed: () {
+                      AuthenticationService()
+                          .signIn(
+                              email: emailController.text.trim(),
+                              password: passwordController.text.trim())
+                          .then(
+                        (result) async {
+                          if (result == null) {
+                            globalUserAccount = await getUserAccountByEmail(
+                                emailController.text.trim());
+                            Navigator.pushReplacement(
+                                context,
+                                MaterialPageRoute(
+                                    builder: (context) => HomePage()));
+                          } else {
+                            // Handling error display
+                            ScaffoldMessenger.of(context).showSnackBar(
+                              SnackBar(
+                                content: Text(
+                                  result,
+                                  style: TextStyle(fontSize: 16),
+                                ),
+                              ),
+                            );
+                          }
+                        },
+                      );
+                    },
+                    color: Colors.black,
+                    textColor: Colors.white,
+                  ),
+                ),
+              ),
+              Padding(
+                padding: EdgeInsets.only(bottom: (size.height * .05)),
+                child: AlreadyHaveAnAccountCheck(
+                  login: true,
+                  press: () {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                        builder: (context) {
+                          return SignUpScreen(); //TODO: Change this to redirect to the choose teams page once its setup
+                        },
+                      ),
+                    );
+                  },
+                ),
+              ),
+            ],
           ),
-        ],
+        ),
       ),
     );
   }
